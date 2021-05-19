460ada297744d3e576b943eca3b150675c05d7fa
Olaru Valentin Daniel <vallentindaniel0@gmail.com>
2021-05-19 16:28:22 +0300

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Product;
-
+use App\Models\Product; // select, insert... about table Products
+//use App\Models\Categories;
+//use App\Models\Product_reviews;
 class ProductController extends Controller
 {
+<<<<<<< HEAD
     /**
      * Display a listing of the resource.
      *
@@ -40,4 +42,16 @@ class ProductController extends Controller
     }
 
 
+=======
+    public function index() {
+        $products =  Product::all();
+      //  $categories = Categories::all();
+       // $categories2 = Categories::select()->inRandomOrder()->limit(10)->get();
+        return view('product/index',
+             [
+                'products'=> $products
+             ]
+        );
+    }
+>>>>>>> parent of 2456716 (product controller)
 }
